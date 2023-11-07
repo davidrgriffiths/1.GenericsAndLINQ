995ebec1dfc61bc41199d44f784defc4d9099ae0
davidrgriffiths <150117282+davidrgriffiths@users.noreply.github.com>
2023-11-07 09:40:31 +0000

    Initial commit

---
+++ UBSTechnicalInterview/DictionaryComparer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UBSTechnicalInterview
+{
+	public class DictionaryComparer
+	{
+		public DictionaryComparer() { }
+
+
+		public static bool Compare<T>(ref T dictionary1, ref T dictionary2)
+		{
+			// Test
+		}
+
+
+        public static bool Compare(Dictionary<int, int> dictionary1, Dictionary<int, int> dictionary2)
+		{
+            bool dictionariesAreTheSame = true;
+
+			// Firstly, check to see if the dictionaries are the same instance
+
+			if (dictionary1.Equals(dictionary2))
+			{
+				return true;
+			}
+			else if(dictionary1.Count == dictionary2.Count)
+			{
+				return false;
+            }
+			else
+			{ 
+				foreach (KeyValuePair<int, int> kvp1 in dictionary1)
+				{
+					int kvp2Value = Int32.MinValue;
+
+					if (!dictionary2.TryGetValue(kvp1.Key, out kvp2Value) || kvp2Value != kvp1.Value)
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+        }
+	}
+}
---
+++ UBSTechnicalInterview/Program.cs
@@ -0,0 +1,49 @@
+﻿using UBSTechnicalInterview;
+
+Dictionary<int, int> dictionary1 = new Dictionary<int, int>();
+Dictionary<int, int> dictionary2 = new Dictionary<int, int>();
+
+
+
+dictionary1.Add(1, 0);
+dictionary1.Add(4, 0);
+dictionary1.Add(5, 4);
+dictionary1.Add(7, 2);
+dictionary1.Add(10, -3);
+
+// Dictionaries are physically the same
+Console.WriteLine(DictionaryComparer.Compare(dictionary1, dictionary1));
+
+// Dictionaries have different counts
+dictionary2.Add(0, 0);
+dictionary2.Add(1, 0);
+dictionary2.Add(4, 0);
+dictionary2.Add(5, 4);
+dictionary2.Add(7, 2);
+
+Console.WriteLine(DictionaryComparer.Compare(dictionary1, dictionary2));
+
+// Dictionaries have the same count and same kvps
+dictionary2.Add(10, -3);
+Console.WriteLine(DictionaryComparer.Compare(dictionary1, dictionary2));
+
+// Change this entry dictionary1.Add(0, 0); to entry dictionary1.Add(2, 0)
+dictionary1.Remove(0);
+dictionary1.Add(2,0);
+
+foreach (KeyValuePair<int,int> kvp in dictionary1)
+{
+    Console.WriteLine($"Key: {kvp.Key} Value: {kvp.Value}");
+}
+
+Console.WriteLine(DictionaryComparer.Compare(dictionary1, dictionary2));
+
+
+
+//dictionary2.Add(0, 0);
+//dictionary2.Add(1, 0);
+//dictionary2.Add(2, 3);
+//dictionary2.Add(4, 0);
+//dictionary2.Add(5, 4);
+//dictionary2.Add(7, 2);
+//dictionary2.Add(10, -3);
